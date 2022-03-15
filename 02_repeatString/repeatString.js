92a3a0c5d55c255b0d0e1f8e9d668c29444625c3
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-03-15 02:09:28 -0700

    Solve repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
 
+    if(repeats < 0)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        let stringArray = new Array(repeats);
+
+        for(let i = 0; i < stringArray.length; i++)
+        {
+            stringArray[i] = string;
+        }
+    
+        let finalString = stringArray.join("");
+    
+        return finalString;
+    }
+    
 };
 
 // Do not edit below this line
