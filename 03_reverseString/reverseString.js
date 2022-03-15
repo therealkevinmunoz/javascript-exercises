0a504397c27e0dde99b5f4b552e83c21295df7d1
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-03-15 02:21:25 -0700

    Complete reverseString.js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reverseArray = new Array(string.length);
+
+    for(let i = 0; i < reverseArray.length; i ++)
+    {
+        reverseArray[i] = string.charAt(i);
+    }
+
+   reverseArray.reverse();
+
+   let reverseFinalString = reverseArray.join("");
+
+   return reverseFinalString;
 };
 
 // Do not edit below this line
