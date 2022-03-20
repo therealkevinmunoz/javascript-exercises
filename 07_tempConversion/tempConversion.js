b4d26bcafae7c080b9e504c4dbf1de4c9da9dad1
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-03-20 09:17:06 -0700

    Complete temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,25 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitTemp) {
+
+  let celsiusTemp;
+
+  let fullCelsiusValue = ((fahrenheitTemp - 32) * (5/9))
+
+  celsiusTemp = Math.round(fullCelsiusValue * 10) / 10
+
+  return celsiusTemp;
 
 };
 
-const ctof = function() {
+const ctof = function(celsiusTemp) {
+
+  let fahrenheitTemp;
+
+  fullFahrenheitValue = ((celsiusTemp * (9/5)) + 32);
+
+   fahrenheitTemp = Math.round(fullFahrenheitValue * 10) / 10
+
+
+  return fahrenheitTemp;
 
 };
 
