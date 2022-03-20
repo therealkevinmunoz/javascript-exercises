71064bc8e906aacabab40c49ca2e43720633d998
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-03-20 01:52:18 -0700

    Complete leap year exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let isLeapYear = false;
+
+    if(year % 4 === 0)
+    {
+        if(year % 100 != 0 || (year % 100 === 0 && year % 400 === 0))
+        {
+            isLeapYear = true;
+        }
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
