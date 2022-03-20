b7565a8168a0107c1fea72f40fa0c0589535a740
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-03-20 01:04:54 -0700

    Complete remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput) {
 
+    for(i = 1; i < arguments.length; i++)
+    {
+        for(j = 0; j < arrayInput.length; j++)
+        {
+            if(arguments[i] === arrayInput[j])
+            {
+                arrayInput.splice(j, 1);
+            }
+        }
+    }
+
+    return arrayInput;
 };
 
 // Do not edit below this line
