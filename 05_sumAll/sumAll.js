6b81fc1d93afe8e29c15b5cedac0a4ac78478666
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-03-20 01:27:54 -0700

    Complete sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    if(typeof startNum != 'number' || typeof endNum != 'number' || startNum < 0 || endNum < 0)
+    {
+        return "ERROR";
+    }
+
+    let newStartNum;
+    let newEndNum;
+    let totalSum = 0;
+
+    if(startNum > endNum)
+    {
+        newStartNum = endNum;
+        newEndNum = startNum;
+    } 
+    else
+    {
+        newStartNum = startNum;
+        newEndNum = endNum;
+    }
+
+    for(let i = newStartNum; i <= newEndNum; i++)
+    {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
