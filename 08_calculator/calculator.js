3afbcde1fdb50254ec300c2ec03f2112c7b67eb3
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-06-06 13:53:13 -0700

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((result, newNumber) => newNumber === 0 ? 0: result * newNumber, 1);
 };
 
-const power = function() {
+const power = function(firstNumber, secondNumber) {
 	
+  let result = firstNumber; 
+
+  for(let i = 1; i < secondNumber; i ++)
+  {
+    result *= firstNumber;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  
+  if(number === 0)
+  {
+    return result;
+  }
+  else
+  {
+    for(let i=1; i <= number; i++)
+    {
+      result *= i;
+    }
+
+    return result;
+  }
+  
 };
 
 // Do not edit below this line
